8038a255a045ee0a0c0300a43fb93d2e7c697b16
dew77 <rrocio46@yahoo.es>
2022-01-10 18:33:53 -0500

    Component Header-Compleated

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-scroll";
+
+
+const Header = () => {
+  return (
+   
+    <div id="home" className="header-wraper">
+      <div className="main-info">
+        <div className="white-space">
+        <h1>Nature's Feature Landscaping LLC.</h1>
+        <h3>Garden and lawn care specialists</h3>
+        <br />
+        <Link smooth={true} to="contacts" offset={-110} className="btn-main-offer">Get a Quote</Link>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
